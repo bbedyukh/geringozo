9ef7078052c963df142c8983733531bd18baad20
Bogdan Bedyukh <bogdan.bedyukh@sondeos.com.ar>
2021-01-21 17:25:41 -0300

    Se resuelve tarea

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-const geringozo = function(str){
+const geringozo = function (str) {
 
-    /* YOUR CODE HERE */
+    let splited = str.split("")
+    let string = ''
+    let qfound = false
+    let gfound = false
+
+    splited.forEach(el => {
+        switch (el) {
+            case 'g':
+                return gfound = true
+            case 'q':
+                return qfound = true
+            case 'a':
+                return string += 'apa'
+            case 'i':
+                return string += 'ipi'
+            case 'o':
+                return string += 'opo'
+            case 'e':
+                return string += 'epe'
+            case 'u':
+                if (qfound) {
+                    qfound = false
+                    return string += 'qu'
+                } else if (gfound) {
+                    qfound = false
+                    return string += 'gu'
+                } else {
+                    return string += 'upu'
+                }
+            default:
+                return string += el
+        }
+    })
+
+    console.log("Splited ", string)
+
+    return string
 
 }
 
